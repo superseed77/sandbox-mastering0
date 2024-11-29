d25248ac88fa9d561f4e85032d5609661daae533
fletcher <bigpanel@gmail.com>
2024-11-29 06:07:01 +0100

    min max

--- js/gen_random.js
+++ js/gen_random.js
@@ -1,13 +1,17 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max) + 1;
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function generateRandom() {
+    let min = document.getElementById('min').value;
     let max = document.getElementById('max').value;
     let res = document.getElementById('result');
-    res.textContent = 'Result: ' + getRandomInt(max);
+    res.textContent = 'Result: ' + getRandomIntInclusive(min, max);
 }
 
 let button = document.querySelector('button');
 button.addEventListener('click', generateRandom);
 button.disabled = false;
+
